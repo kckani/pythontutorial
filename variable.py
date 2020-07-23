af9ee22f790f6517d74ba208a66c2eda2910ff1e
kckani <kckani33@gmail.com>
2020-07-23 13:45:50 +0100

    initial project version

---
+++ variable.py
@@ -0,0 +1,16 @@
+x=1
+y=2.5
+name="john"
+a_school=True
+#assignment: give five examples of varible types and assign values to them
+he_is_a_boy=True
+a=5
+name='shade'
+x=3.0
+is_cool=True
+'''this is a python class where we will be learning to code'''
+print (y)
+print (is_cool)
+print (x)
+fav_color=('blue','white','red')
+print (fav_color)
